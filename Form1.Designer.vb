8eb555f856855f7c0106f7b7e85542546bd9ec51
Paitorocxon <33005175+Paitorocxon@users.noreply.github.com>
2019-12-23 18:29:01 +0100

    I DO NOT OWN THE MUSIC!

---
+++ Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.gamescreen = New System.Windows.Forms.Panel()
+        Me.animation = New System.Windows.Forms.Timer(Me.components)
+        Me.ground = New System.Windows.Forms.Timer(Me.components)
+        Me.jumptimer = New System.Windows.Forms.Timer(Me.components)
+        Me.coin = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'gamescreen
+        '
+        Me.gamescreen.BackColor = System.Drawing.Color.Black
+        Me.gamescreen.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gamescreen.Location = New System.Drawing.Point(0, 0)
+        Me.gamescreen.Name = "gamescreen"
+        Me.gamescreen.Size = New System.Drawing.Size(800, 600)
+        Me.gamescreen.TabIndex = 0
+        '
+        'animation
+        '
+        Me.animation.Enabled = True
+        '
+        'ground
+        '
+        Me.ground.Enabled = True
+        Me.ground.Interval = 1
+        '
+        'jumptimer
+        '
+        Me.jumptimer.Enabled = True
+        Me.jumptimer.Interval = 1
+        '
+        'coin
+        '
+        Me.coin.Enabled = True
+        Me.coin.Interval = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 600)
+        Me.Controls.Add(Me.gamescreen)
+        Me.MinimumSize = New System.Drawing.Size(816, 639)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Snic ze he... you know..."
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents gamescreen As Panel
+    Friend WithEvents animation As Timer
+    Friend WithEvents ground As Timer
+    Friend WithEvents jumptimer As Timer
+    Friend WithEvents coin As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,140 @@
+﻿Imports Toecramp_Engine.engine.rendering
+Imports Toecramp_Engine.engine.physics
+Imports System.ComponentModel
+
+Public Class Form1
+
+    Public isgame As Boolean = True
+    Public coins As Integer = 0
+
+    Public Isit As Boolean = True
+    Public walk As Boolean = True
+
+    Public grounding As Integer = 0
+
+    Public l As Integer = 880
+
+    Public render As New Toecramp_Engine.engine.rendering
+    Public physic As New Toecramp_Engine.engine.physics
+    Public sounds As New Toecramp_Engine.MultiSounds
+
+    Public jumpbuffer As Integer = 0
+
+    Public jumping As Boolean = 0
+    Public jumpstate As Integer = 0
+
+    Dim bgthread As New System.Threading.Thread(AddressOf r)
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Isit = False
+        Else
+            Isit = True
+        End If
+    End Sub
+
+    Public Function r()
+        While True
+            If Isit Then
+                render.ImageSize = gamescreen.Size
+                render.re_initialize()
+
+                render.clear()
+                render.draw(My.Resources.background, 0, 0, 800, 400)
+                render.draw(My.Resources.ground, 0 - grounding, 400, 800, 200)
+                render.draw(My.Resources.ground, 800 - grounding, 400, 800, 200)
+
+                render.draw(My.Resources.collision, l, 360, 32, 32)
+
+                If walk = True Then
+                    render.draw(My.Resources.snic, 200, 360 - jumpbuffer, My.Resources.snic.Width, My.Resources.snic.Height)
+                Else
+                    render.draw(My.Resources.snic_1, 200, 360 - jumpbuffer, My.Resources.snic.Width, My.Resources.snic.Height)
+                End If
+
+                Try
+                    render.FPS_Label("Coins or what ever this is: " & coins)
+                    render.render(gamescreen, render.ImageSize, render.ImageSize, 1)
+                Catch ex As Exception
+                    MsgBox("Game has crashed!")
+                    End
+                End Try
+            End If
+        End While
+        Return 0
+    End Function
+
+    Private Sub animation_Tick(sender As Object, e As EventArgs) Handles animation.Tick
+        If walk Then
+            walk = False
+        Else
+            walk = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        sounds.AddSound("jump", "boing.mp3")
+        bgthread.IsBackground = True
+        bgthread.Start()
+        My.Computer.Audio.Play(My.Resources.snic1, AudioPlayMode.BackgroundLoop)
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+
+        bgthread.Interrupt()
+        bgthread.Abort()
+
+    End Sub
+
+    Private Sub ground_Tick(sender As Object, e As EventArgs) Handles ground.Tick
+        grounding += 1
+        If grounding = 800 Then
+            grounding = 0
+        End If
+    End Sub
+
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyValue
+            Case Keys.Space
+                jump()
+        End Select
+    End Sub
+
+    Public Function jump()
+        If Not jumping Then
+            jumping = True
+            sounds.Play("jump")
+        End If
+        Return 0
+    End Function
+
+    Private Sub jumptimer_Tick(sender As Object, e As EventArgs) Handles jumptimer.Tick
+        If jumping Then
+            If jumpstate = 0 Then
+                If jumpbuffer <= 200 Then
+                    jumpbuffer += 4
+                Else
+                    jumpstate = 1
+                End If
+            ElseIf jumpstate = 1 Then
+                If jumpbuffer >= 0 Then
+                    jumpbuffer -= 4
+                Else
+                    jumpstate = 0
+                    jumping = False
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub coin_Tick(sender As Object, e As EventArgs) Handles coin.Tick
+        If isgame Then
+            l -= 2
+            If l <= -100 Then
+                l = 880
+                coins += 1
+            End If
+        End If
+    End Sub
+End Class
